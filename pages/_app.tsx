3e00e8c0bc72040c9fe1f617c60b89c042db298a
Jon Wilson <jonwilsoncomedy@gmail.com>
2022-08-16 02:30:31 -0500

    adding typography and roboto font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,10 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
