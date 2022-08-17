25f5ba743afa37f63600c96a9482df1650845a4f
Jon Wilson <jonwilsoncomedy@gmail.com>
2022-08-17 02:28:39 -0500

    finishing basic styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,14 @@ import Image from 'next/image';
 import { useSession } from "@inrupt/solid-ui-react";
 import { performLogin } from '../utils';
 import Navbar from '../components/navbar';
-import Button from '@mui/material/Button'
 import LoginIcon from '@mui/icons-material/Login';
-import Typography from '@mui/material/Typography';
+import { Button, Container, Typography } from '@mui/material';
 
 const Home: NextPage = () => {
   const { session } = useSession();
 
   return (
-    <div>
+    <Container maxWidth="md">
       <Image src="/images/blood.png" height={240} width={240} /> 
       <Typography align="center" variant="h2" component="div" gutterBottom>Hello! I'm your period tracker</Typography>
       <Typography variant="h5" component="div" gutterBottom>Let me tell you when to expect your period.</Typography>
@@ -21,7 +20,7 @@ const Home: NextPage = () => {
       <br />
       <Typography variant="h5" component="div" display="inline" gutterBottom>If you do not have a Solid account:</Typography>
       <a href="https://solidproject.org/users/get-a-pod" target="_blank"> Get one</a>
-    </div>
+    </Container>
   )
 }
 
