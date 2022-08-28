12e9cdfa709f54326477539a6e311bee2ca99b20
Jon Wilson <jonwilsoncomedy@gmail.com>
2022-08-28 17:17:49 -0500

    updating UI for logged in user

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ import { handleIncomingRedirect, login, fetch, getDefaultSession } from '@inrupt
 import { getSolidDataset, saveSolidDatasetAt } from "@inrupt/solid-client";
 
 const Home: NextPage = () => {
+  let [loggedIn, setLoggedIn] = useState(false);
   const { session } = useSession();
 
   async function checkLogin() {
@@ -19,6 +20,7 @@ const Home: NextPage = () => {
     })
     const session = getDefaultSession();
     if(session.info.isLoggedIn) {
+      setLoggedIn(true);
       // TODO get solid dataset
       // let dataSet = await getSolidDataset("dataSet URL", {fetch: fetch})
       // see https://docs.inrupt.com/developer-tools/api/javascript/solid-client/modules/resource_solidDataset.html#getsoliddataset
@@ -39,10 +41,21 @@ const Home: NextPage = () => {
       <Typography align="center" component="div" gutterBottom sx={{ fontWeight: 'medium' }} variant="h2">Hello! I'm your period tracker</Typography>
       <Typography variant="h5" component="div" gutterBottom>Let me tell you when to expect your period.</Typography>
       <Typography variant="h5" component="div" gutterBottom>And over time I can make statistics for you.</Typography>
-      <Button endIcon={<LoginIcon />} fullWidth={true} onClick={() => performLogin()} variant="contained">Login with Solid</Button>
-      <br />
-      <Typography variant="h5" component="div" display="inline" gutterBottom>If you do not have a Solid account:</Typography>
-      <a href="https://solidproject.org/users/get-a-pod" target="_blank"> Get one</a>
+      {!loggedIn && (
+        <>
+          <Button endIcon={<LoginIcon />} fullWidth={true} onClick={() => performLogin()} variant="contained">Login with Solid</Button>
+          <br />
+          <Typography variant="h5" component="div" display="inline" gutterBottom>If you do not have a Solid account:</Typography>
+          <a href="https://solidproject.org/users/get-a-pod" target="_blank"> Get one</a>
+        </>
+        )
+      }
+      {loggedIn && (
+        <>
+          <Typography variant="h3" component="div" gutterBottom>You are signed in with WebID:</Typography>
+          <Typography variant="h4" component="div" gutterBottom>{session.info.webId}</Typography>
+        </>
+      )}
     </Container>
   )
 }
